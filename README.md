e59cb59e1ce305cc40b2f0ca73651d21adc38944
Gary Cutler <garycutlerlacrosse@gmail.com>
2023-11-14 15:52:41 -0500

    final and readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # employee-tracker
+
+  # Description
+This is a database application to allow us to track and update our employeee and department info.
+
+
+# tutor
+this project was done with the help of a tutor.
+# link to video 
+https://drive.google.com/file/d/11hJq6ORQrPgIJ70b5rSxZC6-hbqKDbVN/view
+
+
+# github
+https://github.com/GaryCutler/employee-tracker
+
+  * Contact Email:garycutlerlacrosse@gmail.com
--- index.js
+++ index.js
@@ -23,13 +23,12 @@ const db = mysql.createConnection(
       name: "openingMessage",
       message: "what would you like to do",
       choices: [
-        "viewAllEmployees",
+          "viewAllEmployees",
           "viewAllDepartments",
           "viewAllRoles",
           "addADepartment",
           "addARole",
-          "addAEmployee",
-          "updateEmployee",          
+          "addAEmployee",          
           "quit",
       ]
 
@@ -54,10 +53,7 @@ const db = mysql.createConnection(
           break;
         case "addAEmployee":
           addAEmployee();
-          break;
-        case "updateEmployee":
-          updateEmployee();
-          break;       
+          break;      
         case "quit":
           quit();
           break;
@@ -67,8 +63,153 @@ const db = mysql.createConnection(
     }
   })
   } 
+  function viewAllEmployees(){
+    db.query("select * from employees", function(err, res){
+        err? console.log(err): console.table(res), init()
+    })
+  }
   
+  function viewAllDepartments(){
+    db.query("select * from departments", function(err, res){
+        err? console.log(err): console.table(res), init()
+    })
+  }
+
+  function viewAllRoles(){
+    db.query("select * from roles", function(err, res){
+        err? console.log(err): console.table(res), init()
+    })
+  }
+
+  function addADepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "addDepartment",
+            message: "enter a department you want to add"
+
+        }
+    ]).then((departmentResponse)=>{
+        let departmentName = departmentResponse.addDepartment
+        db.query(`insert into departments (department_name) values ("${departmentName}")`, function(err, res){
+            err? console.log(err): viewAllDepartments(), init()
+        })
+    })
+  }
+  function addARole(){
+    db.query("select * from departments", function(err, res){
+        if (err){
+            console.log(err)
+            init()
+        }
+        const departmentList = res.map((department)=>({
+            value: department.department_id, name: department.department_name
+        }))
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "addRole",
+                message: "enter new role"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "what is the salary?"
+            },
+            {
+                type: "list",
+                name: "departmentName",
+                message: "which department does this role belong to",
+                choices: departmentList
+            }
+        ]).then((roleResponses)=>{
+            let departmentChoice = roleResponses.departmentName
+            let roleName = roleResponses.addRole
+            let roleSalary = roleResponses.salary
+            db.query(`insert into roles (job_title, salary, department_id) values ("${roleName}","${roleSalary}","${departmentChoice}")`, function(err, res){
+                err? console.log(err): viewAllRoles(), init()
+            })
+        })
+    })
+  }
+  function addAEmployee() {
+    db.query("SELECT * FROM roles", function (err, results) {
+      if (err) {
+        console.log(err);
+        return init();
+      }
   
+      const roleChoices = results.map((role) => ({
+        value: role.role_id,
+        name: role.role_title,
+      }));
+      db.query("SELECT * FROM employees", function (err, employeeResults) {
+        if (err) {
+          console.log(err);
+          return init();
+        }
   
+        const managerChoices = employeeResults.map((employee) => ({
+          value: employee.employee_id,
+          name: `${employee.first_name} ${employee.last_name}`,
+        }));
+  
+        // Add an option for no manager
+        managerChoices.push({ value: null, name: "No Manager" });
+      ///inquirer
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "firstName",
+            message: "Enter an employee name dood.",
+          },
+          {
+            type: "input",
+            name: "lastName",
+            message: "enter an employee last name dood.",
+          },
+          {
+            type: "list",
+            name: "roleId",
+            message: "wich role are we adding this guy to.",
+            choices: roleChoices,
+          },
+          {
+            type: "list",
+            name: "managerId",
+            message: "Select the employee's manager (or 'No Manager'):",
+            choices: managerChoices,
+          },
+        ])
+        .then((inquirerResponse) => {
+          console.log("dood added: " + inquirerResponse.roleId);
+          const roleId = inquirerResponse.roleId;
+          const empName = inquirerResponse.firstName;
+          const empLast = inquirerResponse.lastName;
+          const managerId = inquirerResponse.managerId;
+          db.query(
+            `INSERT INTO employees 
+                 (first_name, last_name, 
+                  role_id, manager_id) VALUES 
+                  ('${empName}', 
+                  '${empLast}', 
+                  '${roleId}',
+                  '${managerId}')`,
+            function (err, results) {
+              err
+                ? console.log(err)
+                : viewAllEmployees(),
+                init();
+            }
+          );
+        });
+    });
+  });
+  }
+  function quit() {
+    console.log("quitting you");
+    process.exit();
+  }
   init()
 
